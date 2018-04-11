39a2eab6af83ddc3d1f5b68ff87ffa0bd76fdfe5
jpintoriv <jorgepintoriveros@gmail.com>
2018-04-11 15:24:03 -0300

    model for comments

--- URamosProject/comments/models.py
+++ URamosProject/comments/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Coment(models.Model):
+	content = models.TextField(max_length=1024)
+	date = models.DateField(default=timezone.now)
+	isVisible = models.BooleanField(default=True)
+	positivePoints = models.IntegerField(default=0)
+	negativePoints = models.IntegerField(default=0)
