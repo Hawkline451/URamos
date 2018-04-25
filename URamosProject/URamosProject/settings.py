54ebec9d8cd91af0fdb54d12be43f82a46a486c4
jpintoriv <jorgepintoriveros@gmail.com>
2018-04-25 02:24:14 -0300

    change class app for subject app (because class is used for classes in django

--- URamosProject/URamosProject/settings.py
+++ URamosProject/URamosProject/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'class',
+    'subject',
     'comments',
     'moderator',
     'naturalUser',
--- URamosProject/subject/models.py
+++ URamosProject/subject/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from teacher.models import Teacher
 
 # Create your models here.
-class Class(models.Model):
+class Subject(models.Model):
 	code = models.CharField(max_length=16, primary_key=True)
 	department = models.CharField(max_length=64)
 	name = models.CharField(max_length=256)
@@ -13,6 +13,6 @@ class Semester(models.Model):
 	year = models.IntegerField()
 
 class Course(models.Model):
-	_class = models.ForeignKey(Class, on_delete=models.CASCADE)
+	subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
 	semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
 	teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
