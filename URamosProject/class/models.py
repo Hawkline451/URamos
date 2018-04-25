10f1487b072c760689c01c0374123b28dc43696c
jpintoriv <jorgepintoriveros@gmail.com>
2018-04-25 01:57:56 -0300

    relation semester-course-teacher

--- URamosProject/class/models.py
+++ URamosProject/class/models.py
@@ -1,7 +1,18 @@
 from django.db import models
+from teacher.models import Teacher
 
 # Create your models here.
 class Class(models.Model):
-	code = models.CharField(max_length=10, primary_key=True)
+	code = models.CharField(max_length=16, primary_key=True)
+	department = models.CharField(max_length=64)
 	name = models.CharField(max_length=256)
 	note = models.FloatField(default=0)
+
+class Semester(models.Model):
+	name = models.CharField(max_length=16)
+	year = models.IntegerField()
+
+class Course(models.Model):
+	_class = models.ForeignKey(Class, on_delete=models.CASCADE)
+	semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
+	teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
