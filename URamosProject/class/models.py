ba0637e93ea852a645300785ee4f7b43cb561c17
jpintoriv <jorgepintoriveros@gmail.com>
2018-04-25 01:45:22 -0300

    add model for class

--- URamosProject/class/models.py
+++ URamosProject/class/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Class(models.Model):
+	code = models.CharField(max_length=10, primary_key=True)
+	name = models.CharField(max_length=256)
+	note = models.FloatField(default=0)
