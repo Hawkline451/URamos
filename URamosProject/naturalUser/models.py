2d811c6d9db4c181b881967aa721b48f5705f9ec
jpintoriv <jorgepintoriveros@gmail.com>
2018-04-25 02:30:42 -0300

    model for natural user

--- URamosProject/naturalUser/models.py
+++ URamosProject/naturalUser/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class NaturalUser(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True)
+	isLocked = models.BooleanField(default=False)
+
+class LockedUser(models.Model):
+	lockedUser =  models.ForeignKey(NaturalUser, on_delete=models.CASCADE)
+	reasons = models.TextField(max_length=256)
+	date = models.DateField(default=timezone.now)
+	lockedBy = models.ForeignKey(User, on_delete=models.CASCADE)
+
