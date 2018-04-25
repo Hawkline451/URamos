60a34d4afd89a8a68e366ea56ccfdbd515b730f8
jpintoriv <jorgepintoriveros@gmail.com>
2018-04-25 02:26:07 -0300

    edit teacher model

--- URamosProject/teacher/models.py
+++ URamosProject/teacher/models.py
@@ -4,5 +4,4 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Teacher(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True)
-	name = models.CharField(max_length=64)
 	note = models.FloatField(default=0)
