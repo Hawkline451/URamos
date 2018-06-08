9ce9a3e20c0f1a285d125bef679d68b67d33e8b4
Jorge Pinto <jorgepinto@iMac-de-Ricardo.local>
2018-06-08 01:25:39 -0400

    add fetch to auth

--- frontend/src/components/Auth/index.js
+++ frontend/src/components/Auth/index.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
 
 class Auth extends Component {
+
 	render(){
-		console.log("hola");
+		console.log(this.props['ticket'])
+		fetch('https://www.u-cursos.cl/upasaporte/login?servicio=uramos&ticket='+this.props['ticket'])
 		return(
 			"holi"
 			);
