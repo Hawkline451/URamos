2930080e45314b0a0aef54a8a96da57106a421a7
Jorge Pinto <jorgepinto@iMac-de-Ricardo.local>
2018-07-27 21:45:46 -0400

    redux

--- frontend/src/components/Login/Auth.js
+++ frontend/src/components/Login/Auth.js
@@ -1,4 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router'
+import {login} from  '../../actions/auth'
+import {authErrors, isAuthenticated} from '../../reducers'
+
+const mapStateToProps = (state) => ({
+	  errors: authErrors(state),
+	  isAuthenticated: isAuthenticated(state)
+	});
+
 
 
 class Auth extends Component{
@@ -6,9 +16,22 @@ class Auth extends Component{
 		rut: null
 	}
 
+	
+
 	componentDidMount(){
 		this.setState({rut: this.props.match.params.rut});
-		
+		var rut = this.props.match.params.rut
+
+		const mapStateToProps = (state) => ({
+		  errors: authErrors(state),
+		  isAuthenticated: isAuthenticated(state)
+		});
+
+
+		const mapDispatchToProps = (dispatch) => {
+		    dispatch(login(rut, rut))
+		}
+		connect(mapStateToProps, mapDispatchToProps)	
 	}
 
 	render(){
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,21 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import createHistory from 'history/createBrowserHistory'
+import { ConnectedRouter } from 'react-router-redux'
+import { Provider } from 'react-redux'
+import configureStore from './store'
+
+const history = createHistory()
+const store = configureStore(history)
+
+ReactDOM.render(
+	(<Provider store={store}>
+		<ConnectedRouter history={history}>
+			<App />
+		</ConnectedRouter>
+	</Provider>
+	), document.getElementById('root'));
 registerServiceWorker();
 
 if (module.hot) {
