0c440de073b406261211afdb975e1f83d9d26327
Jorge Pinto <jorgepinto@iMac-de-Ricardo.local>
2018-07-28 18:13:12 -0400

    edit token-auth

--- URamosProject/URamosProject/utils.py
+++ URamosProject/URamosProject/utils.py
@@ -1,7 +1,9 @@
 from login.serializers import UserSerializer
+from naturalUser.models import NaturalUser
 
 def my_jwt_response_handler(token, user=None, request=None):
+	natU = NaturalUserSerializer(NaturalUser.objects.get(user=request.user))
 	return {
 		'token': token,
-		'user': UserSerializer(user, context={'request': request}).data
+		'user': natU.data
 	}
