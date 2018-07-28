4829ab20a8b3d5b0990f95eeb288a9f43312de00
Jorge Pinto <jorgepinto@iMac-de-Ricardo.local>
2018-07-28 18:47:03 -0400

    bug fixed

--- URamosProject/URamosProject/utils.py
+++ URamosProject/URamosProject/utils.py
@@ -1,11 +1,8 @@
 from login.serializers import UserSerializer
-from naturalUser.models import NaturalUser
-from naturalUser.serializers import NaturalUserSerializer
-
 
 def my_jwt_response_handler(token, user=None, request=None):
-	natU = NaturalUserSerializer(NaturalUser.objects.get(user=request.user))
+
 	return {
 		'token': token,
-		'user': natU.data
+		'user': UserSerializer(user, context={'request': request}).data
 	}
--- URamosProject/login/serializers.py
+++ URamosProject/login/serializers.py
@@ -7,7 +7,7 @@ class UserSerializer(serializers.ModelSerializer):
 
 	class Meta:
 		model = User
-		fields = ('username','first_name','last_name')
+		fields = ('first_name','last_name')
 
 		
 class UserSerializerWithToken(serializers.ModelSerializer):
