23c9e3a9053d02e7a48fab46d975c20bdb1e7186
Jorge Pinto <jorgepinto@iMac-de-Ricardo.local>
2018-07-28 17:49:02 -0400

    add serializer to naturaluser

--- URamosProject/naturalUser/views.py
+++ URamosProject/naturalUser/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from .models import NaturalUser
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .serializers import NaturalUserSerializer
 
 
 # Create your views here.
 
 @api_view(['GET'])
 def info_user(request):
-	serializer = NaturalUser.objects.get(user=request.user)
+	serializer = NaturalUserSerializer(NaturalUser.objects.get(user=request.user))
 	return Response(serializer.data)
