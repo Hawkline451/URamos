1b693af1a67258b829549086401b4e0d2320ca7a
Jorge Pinto <jorgepinto@iMac-de-Ricardo.local>
2018-07-28 18:15:32 -0400

    impor serialixers

--- URamosProject/URamosProject/utils.py
+++ URamosProject/URamosProject/utils.py
@@ -1,5 +1,7 @@
 from login.serializers import UserSerializer
 from naturalUser.models import NaturalUser
+from naturalUser.serializers import NaturalUserSerializer
+
 
 def my_jwt_response_handler(token, user=None, request=None):
 	natU = NaturalUserSerializer(NaturalUser.objects.get(user=request.user))
