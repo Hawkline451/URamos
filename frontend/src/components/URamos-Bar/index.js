829047fdfed050120e1b36c9bfae226fc2be8283
jorge <jorgepintoriveros@gmail.com>
2018-07-30 13:40:34 -0400

    edit django and navbar

--- frontend/src/components/URamos-Bar/index.js
+++ frontend/src/components/URamos-Bar/index.js
@@ -16,36 +16,45 @@ class UramosBar extends Component {
       this.state = { 
         user: JSON.parse(localStorage.getItem('user')),
         normal_user: JSON.parse(localStorage.getItem('normal_user')),
-        moderator_info: ''
+        user_info: '',
+        mod_info: ''
       };
   }
 
   componentDidMount(){
-    fetch('http://142.93.4.35:3000/auth/current_user/', {
-      headers: {
-        Authorization: `JWT ${localStorage.getItem('token')}`
-      }
-    })
-    .then(res => res.json())
-    .then(json => {
-          localStorage.setItem('normal_user', JSON.stringify(json));
-
-        this.setState({normal_user:json});
-    });
-    this.setState({user: JSON.parse(localStorage.getItem('user')),
-      normal_user: JSON.parse(localStorage.getItem('normal_user')),
-        })
-
-    const infMod = (<Button color="inherit" href={'/moderar'}>
-                    Cursos a Moderar
-                  </Button>);
-    
     if(localStorage.getItem('isLogged') === 'true'){
+      fetch('http://142.93.4.35:3000/auth/current_user/', {
+        headers: {
+          Authorization: `JWT ${localStorage.getItem('token')}`
+        }
+      })
+      .then(res => res.json())
+      .then(json => {
+            localStorage.setItem('normal_user', JSON.stringify(json));
+
+          this.setState({normal_user:json});
+      });
+
+      this.setState({user: JSON.parse(localStorage.getItem('user')),
+            normal_user: JSON.parse(localStorage.getItem('normal_user')),
+      });
+
+      const infMod = (<Button color="inherit" href={'/moderar'}>
+                    Cursos a Moderar
+                    </Button>
+                  );
+      const user = (<Button color="inherit" href={'/evaluacion'}>
+                        Evaluar
+                      </Button>
+                    );
+      this.setState({user_info:user})
       if(this.state.user.isModerator){
-        this.setState({moderator_info:infMod})
+        this.setState({mod_info:infMod})
       }
+
     }
-    console.log(this.state.moderator_info);
+   
+    
   }
 
 
@@ -58,9 +67,8 @@ class UramosBar extends Component {
               <Link to="/">URamos</Link>
             </Typography>
             <div className="buttons">
-              <Button color="inherit" href={'/evaluacion'}>
-                Evaluar
-              </Button>{this.state.moderator_info}
+              {this.state.user_info}
+              {this.state.mod_info}
               <Button color="inherit" href={'/busqueda'}>
                 Todos los cursos
               </Button>
