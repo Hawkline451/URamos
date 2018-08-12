2f75b99559e43a8b32c0c82ce34b102ad7bf696d
jorge <jorgepintoriveros@gmail.com>
2018-08-12 01:49:04 -0300

    actions

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,41 +1,37 @@
 
-export const authStatus = {
+export const AUTHSTATUS = {
 	LOGGED_IN: 'LOGGED_IN',
 	LOGGED_OUT: 'LOGGED_OUT'
 }
 
-export const JWTStatus = {
+export const JWTSTATUS = {
 	WITHOUT_JWT: 'WITHOUT_JWT',
 	JWT_UPDATED: 'JWT_UPDATED',
 	JWT_UNDATCHED: 'JWT_UNDATCHED',
 }
 
-export const login => (user, naturalUser) => ({
+export const login = (user, naturalUser) => ({
 	type: 'LOGIN',
-	first_name: user.first_name,
-	last_name: user.last_name,
-	nickname: naturalUser.nickname,
-	isLocked: naturalUser.isLocked,
-	isModerator: naturalUser.isModerator,
-	isTeacher: naturalUser.isTeacher,
-	teacherName: naturalUser.teacherName
+	user,
+	naturalUser
+	
 });
 
-export const logout => () => ({
+export const logout = () => ({
 	type: 'LOGOUT'
 });
 
-export const setJWT => jwt => ({
+export const setJWT = jwt => ({
 	type: 'SET_JWT',
 	jwt
 });
 
-export const setAuthStatus => auth_status => ({
+export const setAuthStatus = auth_status => ({
 	type: 'SET_AUTH_STATUS',
 	auth_status
 })
 
-export const setJWTStatus => jwt_status => ({
+export const setJWTStatus = jwt_status => ({
 	type: 'SET_JWt_STATUS',
 	jwt_status
 })
