c070d08b6444eca1ee885e84deea787201bc265c
jorge <jorgepintoriveros@gmail.com>
2018-08-12 19:23:31 -0300

    update reducers

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,13 +1,8 @@
 import { combineReducers } from 'redux';
 import JWTStatus from './jwtStatus';
-import jwt from './jwt';
 import authStatus from './authStatus';
-import user from './user';
-
 
 export default combineReducers({
 	JWTStatus,
-	jwt,	
 	authStatus,
-	user
 });
--- frontend/src/reducers/jwtStatus.js
+++ frontend/src/reducers/jwtStatus.js
@@ -2,7 +2,7 @@ import { JWTSTATUS } from '../actions';
 
 const JWTStatus = (state=JWTSTATUS.WITHOUT_JWT, action) => {
 	switch (action.type){
-		case 'SET_JWt_STATUS':
+		case 'SET_JWT_STATUS':
 			return action.jwt_status;
 		default:
 			return state;
