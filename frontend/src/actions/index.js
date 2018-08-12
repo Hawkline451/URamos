43d77af7ed22ee523023461439b7c556d90b8858
jorge <jorgepintoriveros@gmail.com>
2018-08-12 19:23:49 -0300

    update actions

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -10,33 +10,12 @@ export const JWTSTATUS = {
 	JWT_UNDATCHED: 'JWT_UNDATCHED',
 }
 
-export const loginUser = (user) => ({
-	type: 'LOGIN_USER',
-	user,
-	
-});
-
-export const loginNaturalUser = (naturalUser) => ({
-	type: 'LOGIN_NATURAL_USER',
-	naturalUser
-
-})
-
-export const logout = () => ({
-	type: 'LOGOUT'
-});
-
-export const setJWT = jwt => ({
-	type: 'SET_JWT',
-	jwt
-});
-
 export const setAuthStatus = auth_status => ({
 	type: 'SET_AUTH_STATUS',
 	auth_status
 })
 
 export const setJWTStatus = jwt_status => ({
-	type: 'SET_JWt_STATUS',
+	type: 'SET_JWT_STATUS',
 	jwt_status
 })
