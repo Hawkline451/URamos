4837017339907d783932e3cc7621213088863b75
Jorge Pinto <jorgepinto@iMac-de-Ricardo.local>
2018-08-16 22:40:57 -0300

    sacar lista de cursos

--- frontend/src/components/CursosEvaluar/CoursesList.js
+++ frontend/src/components/CursosEvaluar/CoursesList.js
@@ -2,13 +2,10 @@ import React from "react";
 import {Redirect} from "react-router-dom";
 import {withStyles} from "@material-ui/core/styles";
 import axios from 'axios';
-import data from '../../cursosTest1.json';
 
 class CoursesList extends React.Component {
     render() {
-        data.map(function (datita) {
-            console.log(datita)
-        });
+        
         return <div></div>
     }
 }
