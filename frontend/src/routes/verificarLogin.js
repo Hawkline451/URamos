8b9e114eeaedb8d146ceacaabc586e089485eb98
jpintoriv <jorgepintoriveros@gmail.com>
2018-08-19 15:42:31 -0300

    redirect con permisos

---
+++ frontend/src/routes/verificarLogin.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { AUTHSTATUS } from '../actions';
+
+
+
+
+const mapStateToProps = state =>{
+	return{
+		isLogged: state.authStatus === AUTHSTATUS.LOGGED_IN
+	};
+};
+
+
+class VerificarLogin extends Component{
+	
+	render(){
+		if(this.props.isLogged){
+			return (<this.props.comp/>);
+		}else{
+			return (<Redirect to='/'/>)
+		}
+	}
+}
+
+
+export default connect(mapStateToProps)(VerificarLogin);
