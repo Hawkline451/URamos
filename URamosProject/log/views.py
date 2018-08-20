6b4c2e9827d6d4db016ec4b2c23fb48cad5d4f6a
Alexandra <alexasofia@hotmail.com>
2018-08-20 14:46:53 -0300

    fixed order by

--- URamosProject/log/views.py
+++ URamosProject/log/views.py
@@ -9,6 +9,6 @@ from .models import Record
 class LogSearch(View):
     def post(self, request):
         lengthList = 20
-        logList = Record.objects.all().order_by('date')[:lengthList].values('firstComment', 'secondComment', 'typeRecord')
+        logList = Record.objects.all().order_by('date', 'id')[:lengthList].values('firstComment', 'secondComment', 'typeRecord')
         json_data = json.dumps(list(logList), cls=DjangoJSONEncoder)
         return HttpResponse(json_data, content_type='application/json')
